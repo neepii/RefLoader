d9feab58b97fbf01b24b3aa0c529406199184e82
neepi <nikita98351@gmail.com>
2024-07-11 18:03:16 +0300

    added file search feature

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-FLAGS := -Wall
+FLAGS := -Wall -g
 SRC := $(shell C:\\msys64\\usr\\bin\\find.exe ./src -name *.c)
 OBJ := $(patsubst ./src/%.c,%.o,$(SRC))
-LIB := -IC:/msys64/mingw64/include/SDL2 -Dmain=main -LC:/msys64/mingw64/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf
+LIB := -IC:/msys64/mingw64/include/SDL2 -Dmain=main -LC:/msys64/mingw64/lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf -lcomdlg32
 EXEC_NAME := exec
 # -mwindows to disable cmd output
 
--- src/init.c
+++ src/init.c
@@ -2,26 +2,30 @@
 #include <SDL2/sdl_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_syswm.h>
+#include <SDL2/SDL_mouse.h>
 #include <windows.h>
 #include <commdlg.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#define MAX_TEXT_LEN 512
 
-#ifndef UNICODE
-#define UNICODE
-#endif
 
 static SDL_Window * mwindow;
 static SDL_Renderer * mrender;
 static SDL_Texture * mtexture;
+static SDL_Texture * foldertex;
 static SDL_Surface * msurface;
+static SDL_Surface * foldersrf;
 
 
-static const SDL_Rect textbox = {25,25,550,30};
-static const SDL_Rect buttondialog = {470,25,30,30};
+static SDL_Rect textbox = {25,25,500,25};
+static SDL_Rect buttondialog = {550,12,50,50};
 static const int mwW = 640;
 static const int mwH = 480;
+
+bool inpFlag;
+int inpLen;
 
 int exit_code = 0;
 
@@ -35,7 +39,7 @@ HWND getHWND(SDL_Window * window) {
     SDL_GetWindowWMInfo(window, &wminfo);
 
     return wminfo.info.win.window;
-}
+};
 
 
 bool CH_InitSDL() {
@@ -56,31 +60,41 @@ bool CH_InitSDL() {
 
     return EXIT_SUCCESS;
 
-}
+};
 
 static void update(void) {
-
-
     SDL_Rect Rects[2] = {textbox, buttondialog};
 
     SDL_SetRenderDrawColor(mrender, 255, 255, 255, 255);
     SDL_RenderClear(mrender);
 
     SDL_SetRenderDrawColor(mrender, 205,205,205,255);
-    SDL_RenderDrawRects(mrender,Rects, 2);  
+    SDL_RenderDrawRects(mrender,Rects, 2);
 
+
+    SDL_Surface * tempsrf = IMG_Load("res/folder.png");
+    foldersrf = SDL_ConvertSurface(tempsrf, msurface->format, 0);
+    SDL_FreeSurface(tempsrf);
+    foldertex = SDL_CreateTextureFromSurface(mrender, foldersrf);
+    SDL_RenderCopy(mrender, foldertex, NULL, &buttondialog);
+
+    SDL_DestroyTexture(foldertex);
     SDL_RenderPresent(mrender);
-}
+};
 
 void CH_Quit() {
     IMG_Quit();
     TTF_Quit();
     SDL_Quit();
-}
+};
+
+
+
+
 
 bool CH_CreateMenu(char* inpDest) {
 
-    int inpLen = 0;
+    inpLen = 0;
     mwindow = SDL_CreateWindow("Crosshairy", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, mwW, mwH, SDL_WINDOW_OPENGL);
     mrender = SDL_CreateRenderer(mwindow, -1,SDL_RENDERER_SOFTWARE);
     mtexture = SDL_CreateTextureFromSurface(mrender, msurface);
@@ -90,7 +104,7 @@ bool CH_CreateMenu(char* inpDest) {
     };
     msurface = SDL_GetWindowSurface(mwindow);
 
-    inputtext = (char*) malloc(sizeof(char) * 100);
+    char inputtext[MAX_TEXT_LEN];
     memset(inputtext, 0, strlen(inputtext));
 
     SDL_Color black = {0,0,0};
@@ -114,30 +128,10 @@ bool CH_CreateMenu(char* inpDest) {
     bool done = false;
     bool loop = true;
 
-    
-
-    OPENFILENAMEA dialog;
-    char * titledialog = "OPEN";
-    TCHAR szfile[100] = {0};
-    ZeroMemory(&dialog, sizeof dialog);
-    dialog.lStructSize = sizeof(dialog);
-    dialog.hwndOwner = getHWND(mwindow);
-    dialog.lpstrFile = szfile;
-    dialog.nMaxFile = sizeof(szfile);
-    dialog.lpstrFilter = "All\0*.*\0Text\0*.TXT\0";
-    dialog.nFilterIndex = 1;
-    dialog.lpstrFileTitle = titledialog;
-    dialog.nMaxFileTitle = sizeof(titledialog);
-    dialog.lpstrInitialDir = NULL;
-    dialog.Flags = OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST;
-
-    // if (GetOpenFileName(&dialog)) {
-
-    // }
 
     while(loop) {
         SDL_Event eve;
-        bool inpFlag = false;
+        inpFlag = false;
         while(SDL_PollEvent(&eve)) {
             switch (eve.type)
             {
@@ -146,35 +140,79 @@ bool CH_CreateMenu(char* inpDest) {
                 exit_code = 1;
                 break;
             case SDL_TEXTINPUT:
-                strcat(inputtext, eve.text.text);
-                inpLen++;
-                inpFlag = true;
+                if (inpLen + 1 <= MAX_TEXT_LEN) {
+                    strcat(inputtext, eve.text.text);
+                    inpLen++;
+                    inpFlag = true;
+                }
                 break;
             case SDL_KEYDOWN:
-                if (eve.key.keysym.sym == SDLK_BACKSPACE && inpLen > 0) {
+                if (eve.key.keysym.sym == SDLK_BACKSPACE && inpLen >= 0) {
                     inputtext[inpLen-1] = '\0';
                     inpLen--;
                     inpFlag = true;
                 }
+
                 else if (eve.key.keysym.sym == SDLK_RETURN) {
+                    if (*inputtext == '"' && inputtext[inpLen-1] == '"') {
+                        memmove(inputtext, inputtext+1, strlen(inputtext)-2);
+                        inputtext[strlen(inputtext)-2 ]= '\0';
+                    }
                     done = true;
                     inpFlag =true;
+                    
                 }
                 else if (eve.key.keysym.sym == SDLK_c && KMOD_CTRL) {
                     SDL_SetClipboardText(inputtext);
                     inpFlag = true;
                 }
                 else if (eve.key.keysym.sym == SDLK_v && KMOD_CTRL) {
-                    strcpy(inputtext, SDL_GetClipboardText());
-                    inpFlag = true;
+                    char * cliptext = SDL_GetClipboardText();
+                    int clipLen = strlen(cliptext);
+                        if ((inpLen+clipLen) < MAX_TEXT_LEN) {
+                            strncat(inputtext, cliptext, clipLen);
+                            inpLen += clipLen;
+                            inpFlag = true;
+                        }
                 }
-
+            case SDL_MOUSEBUTTONDOWN:
+                int x,y;
+                SDL_GetMouseState(&x, &y);
+                if (x >= buttondialog.x &&
+                    y >= buttondialog.y &&
+                    x <= (buttondialog.w +buttondialog.x) &&
+                    y <= (buttondialog.h + buttondialog.y)) {
+                        OPENFILENAME dialog; 
+                        CHAR szfile[100];
+                        ZeroMemory(&dialog, sizeof(dialog));
+                        dialog.lStructSize = sizeof(dialog);
+                        dialog.hwndOwner = NULL;
+                        dialog.lpstrFile = szfile;
+                        dialog.lpstrFile[0] = '\0';
+                        dialog.nMaxFile = sizeof(szfile);
+                        dialog.lpstrFilter = "All Files\0*.*\0\0";
+                        dialog.nFilterIndex = 1;
+                        dialog.lpstrFileTitle = NULL;
+                        dialog.nMaxFileTitle = 0;
+                        dialog.lpstrInitialDir = NULL;
+                        dialog.Flags = OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST;
+                        if (GetOpenFileName(&dialog)) {
+                            char * path = dialog.lpstrFile;
+                            int pathLen = strlen(path);
+                            if ((inpLen+pathLen) < MAX_TEXT_LEN) {
+                                strncat(inputtext, path, pathLen);
+                                inpLen += pathLen;
+                                inpFlag = true;
+                            }
+                        }
+                        
+                    }
             default:
                 break;
             }
         }
 
-        if (inputtext && inpFlag) {
+        if (inpLen && inpFlag) {
             if (done) {
                 strcpy(inpDest, inputtext);
                 loop = false;
@@ -200,8 +238,6 @@ bool CH_CreateMenu(char* inpDest) {
     } // loop
 
     SDL_StopTextInput();
-    memset(inputtext, 0, strlen(inputtext));
-    free(inputtext);
     TTF_CloseFont(font);
     SDL_DestroyTexture(mtexture);
     SDL_DestroyWindowSurface(mwindow);
